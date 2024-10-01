dccacf9f77379eaf3bfbf0746b2a65188593d9da
Quang Huy <kaheetonaa@gmail.com>
2024-10-01 14:02:10 +0200

    stylize

--- result.py
+++ result.py
@@ -65,105 +65,105 @@ result=pd.DataFrame(list(collection.find()))
 
 with container1:
     st.html("<img src='https://raw.githubusercontent.com/kaheetonaa/streamlit_quizz_template_euth/refs/heads/main/asset/logo.png' class='center'/>")
+    st.markdown(""" ___""")
+    if 'question' in result:
+    
 
-if 'question' in result:
-   
+        result0=result[result['question']==0].groupby(['selection','school']).count().reset_index()
+        result1=result[result['question']==1].groupby(['selection','school']).count().reset_index()
+        result2=result[result['question']==2].groupby(['selection','school']).count().reset_index()
 
-    result0=result[result['question']==0].groupby(['selection','school']).count().reset_index()
-    result1=result[result['question']==1].groupby(['selection','school']).count().reset_index()
-    result2=result[result['question']==2].groupby(['selection','school']).count().reset_index()
+        #color
+        domain = ["🇮🇹Italy", "🇵🇹Portugal", "🇷🇴Romania", "🇸🇰Slovakia", "🇪🇸Spain"]
+        range_ = ['#B9F3E3', '#F5716C', '#F5CC81', '#6799A3', '#F5996F']
 
-    #color
-    domain = ["🇮🇹Italy", "🇵🇹Portugal", "🇷🇴Romania", "🇸🇰Slovakia", "🇪🇸Spain"]
-    range_ = ['#B9F3E3', '#F5716C', '#F5CC81', '#6799A3', '#F5996F']
+        click = alt.selection_point(encodings=['color'])
 
-    click = alt.selection_point(encodings=['color'])
-
-    color = alt.condition(
-    click,
-    alt.Color('school:N').scale(domain=domain,range=range_),
-    alt.value('lightgray')
-)
-
-    chart1A = alt.Chart(result0,title='question 01').mark_bar().encode(
-        x='selection',
-        xOffset='selection',
-        y='count()',
-        color=color
-    ).add_params(
-        click
+        color = alt.condition(
+        click,
+        alt.Color('school:N').scale(domain=domain,range=range_),
+        alt.value('lightgray')
     )
 
-    chart1B = alt.Chart(result0).mark_bar(
-    ).encode(
-        y='selection',
-        x='count()',
-        row='school',
-        color=alt.condition(click, 'school', alt.value('lightgray'))
-    ).add_params(
-        click
-    )
-    chart1=alt.hconcat(chart1A,chart1B)
-    #alt.condition(click, 'Origin', alt.value('lightgray'))
-    chart2A= alt.Chart(result1,title='question 02').mark_bar(
-    ).encode(
-        x='selection',
-        y='count()',
-        color=alt.condition(click, 'school', alt.value('lightgray'))
-    ).add_params(
-        click
-    )
+        chart1A = alt.Chart(result0,title='question 01').mark_bar().encode(
+            x='selection',
+            xOffset='selection',
+            y='count()',
+            color=color
+        ).add_params(
+            click
+        )
 
-    chart2B = alt.Chart(result1).mark_bar(
-    ).encode(
-        y='selection',
-        x='count()',
-        row='school',
-        color=alt.condition(click, 'school', alt.value('lightgray'))
-    ).add_params(
-        click
-    )
-    chart2=alt.hconcat(chart2A,chart2B)
+        chart1B = alt.Chart(result0).mark_bar(
+        ).encode(
+            y='selection',
+            x='count()',
+            row='school',
+            color=alt.condition(click, 'school', alt.value('lightgray'))
+        ).add_params(
+            click
+        )
+        chart1=alt.hconcat(chart1A,chart1B)
+        #alt.condition(click, 'Origin', alt.value('lightgray'))
+        chart2A= alt.Chart(result1,title='question 02').mark_bar(
+        ).encode(
+            x='selection',
+            y='count()',
+            color=alt.condition(click, 'school', alt.value('lightgray'))
+        ).add_params(
+            click
+        )
 
-    chart3A= alt.Chart(result2,title='question 03').mark_bar(
-    ).encode(
-        x='selection',
-        y='count()',
-        color=alt.condition(click, 'school', alt.value('lightgray'))
-    ).add_params(
-        click
-    )
+        chart2B = alt.Chart(result1).mark_bar(
+        ).encode(
+            y='selection',
+            x='count()',
+            row='school',
+            color=alt.condition(click, 'school', alt.value('lightgray'))
+        ).add_params(
+            click
+        )
+        chart2=alt.hconcat(chart2A,chart2B)
 
-    chart3B = alt.Chart(result2).mark_bar(
-    ).encode(
-        y='selection',
-        x='count()',
-        row='school',
-        color=alt.condition(click, 'school', alt.value('lightgray'))
-    ).add_params(
-        click
-    )
-    chart3=alt.hconcat(chart3A,chart3B) 
+        chart3A= alt.Chart(result2,title='question 03').mark_bar(
+        ).encode(
+            x='selection',
+            y='count()',
+            color=alt.condition(click, 'school', alt.value('lightgray'))
+        ).add_params(
+            click
+        )
 
-    chart = alt.vconcat(chart1,chart2,chart3).configure(background='white',countTitle='number').configure_axis(
-    labelFontSize=16,
-    titleFontSize=16,
-    labelFont='comfortaa',
-    titleFont='comfortaa',
-    ).configure_legend(
-    labelFontSize=16,
-    titleFontSize=16,
-    labelFont='comfortaa',
-    titleFont='comfortaa',
-    ).configure_axisY(title=None).configure_headerRow(
-    labelFont='comfortaa',
-    labelFontSize=12,
-    titleFont='comfortaa',
-    titleFontSize=16
-    )
+        chart3B = alt.Chart(result2).mark_bar(
+        ).encode(
+            y='selection',
+            x='count()',
+            row='school',
+            color=alt.condition(click, 'school', alt.value('lightgray'))
+        ).add_params(
+            click
+        )
+        chart3=alt.hconcat(chart3A,chart3B) 
 
-    col1,col2 = st.columns([1,4])
-    with col2:
-        st.altair_chart(chart,theme=None)
-else:
-    st.write('No answers yet')
+        chart = alt.vconcat(chart1,chart2,chart3).configure(background='white',countTitle='number').configure_axis(
+        labelFontSize=16,
+        titleFontSize=16,
+        labelFont='comfortaa',
+        titleFont='comfortaa',
+        ).configure_legend(
+        labelFontSize=16,
+        titleFontSize=16,
+        labelFont='comfortaa',
+        titleFont='comfortaa',
+        ).configure_axisY(title=None).configure_headerRow(
+        labelFont='comfortaa',
+        labelFontSize=12,
+        titleFont='comfortaa',
+        titleFontSize=16
+        )
+
+        col1,col2 = st.columns([1,4])
+        with col2:
+            st.altair_chart(chart,theme=None)
+    else:
+        st.write('No answers yet')
