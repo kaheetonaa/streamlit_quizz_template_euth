d763587e3b2974a1f37cda5fff49759c259000e3
Quang Huy <kaheetonaa@gmail.com>
2024-10-01 14:28:45 +0200

    stylize

--- result.py
+++ result.py
@@ -103,7 +103,7 @@ with container1:
             color=alt.condition(click, 'school', alt.value('lightgray'))
         ).add_params(
             click
-        ).properties(height=300)
+        ).properties(height=300,width=500)
         chart1=alt.hconcat(chart1A,chart1B)
         #alt.condition(click, 'Origin', alt.value('lightgray'))
         chart2A= alt.Chart(result1,title='question 02').mark_bar(
