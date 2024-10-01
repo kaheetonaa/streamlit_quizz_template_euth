afa18a8ec397ff0ee6fd752344080a52fed5d070
Quang Huy <kaheetonaa@gmail.com>
2024-10-01 14:13:54 +0200

    stylize

--- result.py
+++ result.py
@@ -165,7 +165,7 @@ with container1:
         labelFontSize=12,
         titleFont='comfortaa',
         titleFontSize=16
-        ).configure_bar(width=10)
+        ).configure_bar(width=50)
 
         col1,col2 = st.columns([1,4])
         with col2:
