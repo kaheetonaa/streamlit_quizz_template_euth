ce9a1eb5e754aed018592b492c83a439d79b75f4
Quang Huy <kaheetonaa@gmail.com>
2024-10-01 12:29:42 +0200

    stylize

--- result.py
+++ result.py
@@ -7,7 +7,7 @@ def run():
     st.set_page_config(
         page_title="🌐 EuthMappers quizz result",
         page_icon="✅",
-        layout="wide"
+        layout="centered"
     )
 
 if __name__ == "__main__":
